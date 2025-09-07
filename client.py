f9b1ff6209162d2b58279568425e9e9cdc20ca3b
lucyfidock <a1884810@adelaide.edu.au>
2025-09-07 18:56:30 +0930

    improving python client server implementation

--- client.py
+++ client.py
@@ -1,35 +1,108 @@
 import asyncio
 import websockets
+import json
 
 class Client:
     def __init__(self):
         self.websocket = None
+        self.client_id = "Guest"
+
+    async def encrypt_message(self):
+        #TODO: set up method of encrypting a message, only encrypt message content, not other json text
+        return 
+    
+    async def decrypt_message(self):
+        # TODO: set up method of decrypting a message
+        return 
+
+    async def signin(self):
+        """Sign into the chat service"""
+        # TODO: implementation to be improved for sign in to increase security 
+        while True:
+            auth_type = input("If you are a new user, please enter 0.\n If you are a returning user, please enter 1:\n")
+
+            if auth_type == '0':
+                auth_type = "Sign-up"
+                break
+            elif auth_type == '1':
+                auth_type = "Sign-in"
+                break
+        
+        # loop to sign in user, will not complete until user signed in successfully 
+        while True:
+            client_username = input("Please enter your username: ")
+            message_data = json.dumps({
+                "type": auth_type,
+                "content": client_username,
+                "recipient": "server",
+                "sender": self.client_id
+            })
+            await self.websocket.send(message_data)
+            
+            try:
+                message = await self.websocket.recv()
+                response = json.loads(message)
+                if response["content"] == "Success":
+                    self.client_id = client_username
+                    return  
+                elif response["content"] == "Unavailable":
+                    print("Username unavailable. ")
+                else:
+                    print("Unexpected response")
+            except websockets.exceptions.ConnectionClosed:
+                print("Connection closed by server")
     
     async def listen_for_messages(self):
-        # listen for incoming messages from server
+        """Listens to incoming messages from the server"""
+        #loop through and print received messages
+        #TODO: error handling for messages with type 'Error'
+
         try:
-            async for message in self.websocket:
-                print(f"Received: {message}")
+            async for json_message in self.websocket:
+                message = json.loads(json_message)
+                print(f"Received: {message['content']} from {message['sender']}")
         except websockets.exceptions.ConnectionClosed:
             print("Connection closed by server")
     
     async def send_messages(self):
-        # send messages from user input
+        """Send messages from user input"""
+
         loop = asyncio.get_event_loop()
         
         while True:
-            # get user input in a non-blocking way
+            # get message 
             message = await loop.run_in_executor(None, input)
+            
+            # check if user wishes to quit
             if message.lower() == 'quit':
                 break
-            await self.websocket.send(message)
+            
+            # get recipient of message 
+            recipient = input("Please enter the username of the recipient of this message, or 'Group' to send to all users: ")
+
+            # format json message data
+            message_data = json.dumps({
+                "type": "chat",
+                "content": message,
+                "recipient": recipient,
+                "sender": self.client_id
+            })
+
+            # send message over websockets
+            await self.websocket.send(message_data)
     
     async def start(self):
+        # TODO: 1) modify to work over network using ip and port forwarding
+        # 2) need to join server without knowing ip & port ?
+
         uri = "ws://localhost:8765"
         try:
+            # use connect function to establish websocket connection with specified uri
             async with websockets.connect(uri) as websocket:
                 self.websocket = websocket
                 print("Connected to chat server!")
+                await self.signin()
+
                 print("Type messages and press Enter. Type 'quit' to exit.")
                 
                 # run both listening and sending concurrently
@@ -41,5 +114,6 @@ class Client:
             print(f"Connection error: {e}")
 
 if __name__ == "__main__":
+    # initialise client and run
     client = Client()
     asyncio.run(client.start())
--- server.py
+++ server.py
@@ -1,43 +1,127 @@
+
 import asyncio
 import websockets
+import json
 
-# store all connected clients with their IDs
-clients = {}
-next_client_id = 1
+class Server:
+    def __init__(self):
+        self.clients = {}
 
-async def handle_client(websocket):
-    global next_client_id
-    
-    # assign ID to client
-    client_id = next_client_id
-    next_client_id += 1
-    
-    # add client to dictionary
-    clients[client_id] = websocket
-    print(f"Client {client_id} connected. Total clients: {len(clients)}")
-    
-    try:
-        async for message in websocket:
-            print(f"Client {client_id} sent: {message}")
-            
-            # broadcast message to all other clients
-            if clients:
-                send_tasks = []
-                for id, client in clients.items():
-                    if id != client_id: # don't send message back to same client
-                        send_tasks.append(client.send(f"Client {client_id}: {message}"))
+    async def client_sign_up(self, client_id, websocket):
+        """Sign up a new client"""
+        # if client id is already taken, or client attempts to use a restricted username, respond as unavailable
+        if client_id == "group" or client_id in self.clients:
+            await websocket.send(json.dumps({
+                "type": "Server Auth",
+                "content": "Unavailable",
+                "recipient": "Guest",
+                "sender": "Server"
+            }))
+            return None
+        # if client id is available, add user to client list 
+        else:
+            await websocket.send(json.dumps({
+                "type": "Server Auth",
+                "content": "Success",
+                "recipient": client_id,
+                "sender": "Server"
+            }))
+            self.clients[client_id] = websocket
+            print(f"Client {client_id} connected. Total clients: {len(self.clients)}")
+            return client_id
+
+    async def client_sign_in(self, client_id, websocket):
+        """Sign up a client"""
+        # if client id is in client list, sign them in
+        if client_id in self.clients:
+            await websocket.send(json.dumps({
+                "type": "Server Auth",
+                "content": "Success",
+                "recipient": client_id,
+                "sender": "Server"
+            }))
+            print(f"Client {client_id} connected. Total clients: {len(self.clients)}")
+            return client_id
+        # if client id is not in client list, treat as a sign up
+        else:
+            await self.client_sign_up(client_id, websocket)
+
+    async def send_group_message(self, message, client_id):
+        """Broadcast a message to all clients (except for sender)"""
+        # if clients exist in client list, send them the message
+        if self.clients:
+            send_tasks = []
+            # go through all clients and add to task array 
+            for id, client in self.clients.items():
+                if id != client_id:
+                    send_tasks.append(client.send(json.dumps({
+                        "type": "chat",
+                        "content": message["content"],
+                        "recipient": message["recipient"],
+                        "sender": client_id
+                    })))
+                # send all messages at the same time 
                 await asyncio.gather(*send_tasks, return_exceptions=True)
-    except websockets.exceptions.ConnectionClosed:
-        pass
-    finally:
-        # remove client when disconnected
-        del clients[client_id]
-        print(f"Client {client_id} disconnected. Total clients: {len(clients)}")
 
-async def main():
-    print("WebSocket chat server running on ws://localhost:8765")
-    async with websockets.serve(handle_client, "localhost", 8765):
-        await asyncio.Future()  # Run forever
+    async def send_message(self, message, client_id, websocket):
+        """Send a message to a specific client"""
+        # check if message recipient is in client list, if so, send them the message
+        message_recipient = message["recipient"]
+        if message_recipient in self.clients:
+            recipient_ws = self.clients[message_recipient]
+            await recipient_ws.send(json.dumps({
+                "type": "chat",
+                "content": message["content"],
+                "recipient": message["recipient"],
+                "sender": client_id
+            }))
+        # if recipient is not found, notify the sender
+        else:
+            await websocket.send(json.dumps({
+                "type": "Error",
+                "content": "Recipient not found",
+                "recipient": client_id,
+                "sender": "Server"
+            }))
+            
+
+    async def handle_client(self, websocket):
+        """Handle messages from clients"""
+        # initialise client id
+        client_id = "guest"
+
+        try:
+            async for json_message in websocket:
+                message = json.loads(json_message)
+
+                # log all messages received 
+                print(message)
+                
+                if message["type"] == "Sign-in":
+                    client_id = await self.client_sign_in(message["content"], websocket)
+                elif message["type"] == "Sign-up":
+                    client_id = await self.client_sign_up(message["content"], websocket)
+                elif message["type"] == "chat":
+                    if message["recipient"] == "Group":
+                        await self.send_group_message(message, client_id)
+                    else:
+                        await self.send_message(message, client_id, websocket)
+
+        except websockets.exceptions.ConnectionClosed:
+            pass
+        
+        finally:
+            # remove client when disconnected
+            if client_id in self.clients:
+                del self.clients[client_id]
+                print(f"Client {client_id} disconnected. Total clients: {len(self.clients)}")
+
+    async def start(self):
+        """Start running server"""
+        print("WebSocket chat server running on ws://localhost:8765")
+        async with websockets.serve(self.handle_client, "localhost", 8765):
+            await asyncio.Future()  # Run forever
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    server = Server()
+    asyncio.run(server.start())
